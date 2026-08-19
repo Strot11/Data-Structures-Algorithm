ef0ae046c1158ff5f06d16dcd47df294e777b0fe
Strot11 <148188119+Strot11@users.noreply.github.com>
2026-08-19 17:09:50 +0530

    Time: 101 ms (17.23%) | Memory: 73.3 MB (37.16%) - LeetSync

---
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+bool isAvailable(int a,int b,int c,int d,vector<int> &num){
+    for(auto n:num){
+        if(n == a||n==b||n==c||n==d) return false;
+    }
+    return true;
+}
+    int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
+        int m = reservedSeats.size();
+        unordered_map<long long,vector<int>> gp;
+        long long count = 0;
+        sort(reservedSeats.begin(),reservedSeats.end());
+        for(int i=0;i<m;i++){
+            int row = reservedSeats[i][0];
+            int seat = reservedSeats[i][1];
+           gp[row].push_back(seat);
+        }
+       for(auto [key,val]: gp){
+        bool g1 = isAvailable(2,3,4,5,val);
+        bool g2 = isAvailable(4,5,6,7,val);
+        bool g3 = isAvailable(6,7,8,9,val);
+        if(g1 && g3) count+=2;
+        else if(g1 || g2 || g3) count++;
+       }
+        return (n-gp.size())*2 + count; 
+    }
+};
