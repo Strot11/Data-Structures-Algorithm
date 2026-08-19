bf673a9eddde62c31405967d81c84cd2775d5b06
Strot11 <148188119+Strot11@users.noreply.github.com>
2026-08-19 17:39:35 +0530

    Time: 14 ms (91.39%) | Memory: 61.8 MB (54.56%) - LeetSync

--- 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-bool isAvailable(int a,int b,int c,int d,unordered_set<int> &num){
-    if(num.find(a)!=num.end() || num.find(b)!=num.end() || num.find(c)!=num.end() || num.find(d)!=num.end()) return false;
-    return true;
+bool isAvailable(int a,int &num){
+    return ((a&num) == 0);
 }
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
         int m = reservedSeats.size();
-        unordered_map<long long,unordered_set<int>> gp;
+        unordered_map<long long,int> gp;
         long long count = 0;
         for(int i=0;i<m;i++){
             int row = reservedSeats[i][0];
             int seat = reservedSeats[i][1];
-           gp[row].insert(seat);
+            int bit = (1<<seat);
+           gp[row] |= bit;
         }
+        int mask3= 0b1111000000;
+        int mask2 = 0b0011110000;
+        int mask1 = 0b0000111100;
        for(auto [key,val]: gp){
-        bool g1 = isAvailable(2,3,4,5,val);
-        bool g2 = isAvailable(4,5,6,7,val);
-        bool g3 = isAvailable(6,7,8,9,val);
+        bool g1 = isAvailable(val,mask1);
+        bool g2 = isAvailable(val,mask2);
+        bool g3 = isAvailable(val,mask3);
         if(g1 && g3) count+=2;
         else if(g1 || g2 || g3) count++;
        }
