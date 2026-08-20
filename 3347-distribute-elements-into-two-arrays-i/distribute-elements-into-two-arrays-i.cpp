afab0cc65a48199663ec8bee7c085bc34ca65c17
Strot11 <148188119+Strot11@users.noreply.github.com>
2026-08-20 17:33:50 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (66.37%) - LeetSync

---
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> a;
+        vector<int> b;
+        a.push_back(nums[0]);
+        b.push_back(nums[1]);
+        for(int i=2;i<n;i++){
+            if(b.back()<a.back()) a.push_back(nums[i]);
+            else b.push_back(nums[i]);
+        }
+        a.insert(a.end(),b.begin(),b.end());
+        return a;
+    }
+};
