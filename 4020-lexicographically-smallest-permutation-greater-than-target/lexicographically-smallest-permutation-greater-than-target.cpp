48429f40a4aa4a1b02e5aacd9da8cb4dda849363
Strot11 <148188119+Strot11@users.noreply.github.com>
2026-08-27 23:50:27 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (74.52%) - LeetSync

---
+++ 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+string result = "";
+bool solve(int i,vector<int> &alph,string &target,bool &greater,string& ans){
+    if(i == target.length()){
+        if(greater){
+            result = ans;
+            return true;
+        }
+        return false;
+    }
+    for(char ch='a';ch<='z';ch++){
+        if(alph[ch-'a']==0) continue;
+        if(!greater && ch < target[i]) continue;
+        if(ch > target[i]) greater = true;
+        alph[ch-'a']--;
+        ans.push_back(ch);
+        if(solve(i+1,alph,target,greater,ans)) return true;
+        ans.pop_back();
+        alph[ch-'a']++;
+    }
+    return false;
+}
+    string lexGreaterPermutation(string s, string target) {
+        int n = s.length();
+        vector<int> alph(26,0);
+        for(int i=0;i<n;i++){
+            alph[s[i]-'a']++;
+        }
+        string ans = "";
+        bool greater = false;
+        bool can = solve(0,alph,target,greater,ans);
+        if(can) return result;
+        return "";
+    }
+};
