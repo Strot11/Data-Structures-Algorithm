5d8540f5569fcd795c0d5557f592a57c5543a50c
Strot11 <148188119+Strot11@users.noreply.github.com>
2026-09-14 21:17:26 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (11.03%) - LeetSync

---
+++ 866-rectangle-overlap/rectangle-overlap.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
+        int x1 = rec1[0];
+        int y1 = rec1[1];
+        int x2 = rec1[2];
+        int y2 = rec1[3];
+        int a1 = rec2[0];
+        int b1 = rec2[1];
+        int a2 = rec2[2];
+        int b2 = rec2[3];
+
+        if(x2<=a1 || a2<=x1 || b2<=y1 || y2<=b1) return false;
+        return true;
+
+        
+    }
+};
