cee25900dc4c9a3854a1a503d21a65728b42168b
Strot11 <148188119+Strot11@users.noreply.github.com>
2026-09-24 15:14:55 +0530

    Time: 0 ms (100.00%) | Memory: 30.8 MB (98.04%) - LeetSync

---
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+int solve(int num){
+    int sum = 0;
+    while(num>0){
+        sum += num%10;
+        num /=10;
+    }
+    return sum;
+}
+    int smallestIndex(vector<int>& nums) {
+       int n = nums.size();
+       for(int i=0;i<n;i++){
+        if(solve(nums[i]) == i) return i;
+       }
+       return -1;
+    }
+};
